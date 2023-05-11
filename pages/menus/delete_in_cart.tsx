e688fc3d17ee9d9da6ee19c73b17f5e473deca3d
jinsang <jinsang@nhndata.com>
2023-05-11 16:34:06 +0900

    장바구니 삭제 화면 작업

--- pages/menus/delete_in_cart.tsx
+++ pages/menus/delete_in_cart.tsx
@@ -1,4 +1,13 @@
-import { Box, Flex, Spacer, Heading, Text } from '@chakra-ui/react'
+import {
+  Box,
+  Flex,
+  Spacer,
+  Heading,
+  Text,
+  useToast,
+  Center
+} from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
 import { Stack, StackDivider } from '@chakra-ui/react'
 import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
 import { ButtonGroup, Button } from '@chakra-ui/react'
@@ -8,7 +17,7 @@ import Head from 'next/head'
 
 import BackButton from '@/components/BackButton'
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import { FC } from 'react'
 
 import {
@@ -21,15 +30,67 @@ import {
   ACEMaritalStatus
 } from '@jinsang/slimer-react'
 import { getRandomIntInclusive, sendCommonWithPromise } from '@/utils'
+import type { IProduct } from '../../data/types/iproduct-type'
+import ProductCard from '../../components/ProductCard'
+import { createRandomProduct } from '@/data/createRandomProduct'
 
 const title = 'DeleteInCart'
 const DeleteInCart: FC = () => {
-  const randomValueForScreen = getRandomIntInclusive(0, 999).toString()
+  const [randomValueForScreen, setRandomValueForScreen] = useState(0)
   useEffect(() => {
-    const msgForScreen = `>>${title}<< >>${randomValueForScreen}<<`
+    const ranValue = getRandomIntInclusive(0, 999)
+    setRandomValueForScreen(ranValue)
+    const msgForScreen = `>>${title}<< >>${ranValue}<<`
     const params = ACParams.init(ACParams.TYPE.EVENT, msgForScreen)
     sendCommonWithPromise(msgForScreen, params)
   }, [])
+  const randomValue = getRandomIntInclusive(0, 999)
+  const [url, setUrl] = useState<string>(`>>${title}<< >>${randomValue}<<`)
+  const [memberKey, setMemberKey] = useState<string>(
+    `멤버ID >>${randomValue + 0}<<`
+  )
+  // product
+  const [products, setProducts] = useState<IProduct[]>([])
+  const addProduct = useCallback(() => {
+    const randomValue = getRandomIntInclusive(0, 99)
+    setProducts((products) => [createRandomProduct(randomValue), ...products])
+  }, [])
+  const deleteProduct = useCallback(
+    (id: string) => () => {
+      setProducts((products) => products.filter((p) => p.id !== id))
+    },
+    []
+  )
+  const allClearProducts = useCallback(() => {
+    setProducts([])
+  }, [])
+  const toast = useToast()
+  const onSend = useCallback(() => {
+    const params = ACParams.init(ACParams.TYPE.DELCART)
+    params.memberKey = memberKey
+    params.products = []
+    products.map((item) => {
+      params.products?.push(
+        new ACProduct(
+          item.name,
+          item.category,
+          item.price,
+          item.quantity,
+          item.productId,
+          item.optionCodeName
+        )
+      )
+    })
+    sendCommonWithPromise(url, params)
+
+    toast({
+      title: '알림',
+      description: params.memberKey ?? params.type,
+      status: 'success',
+      duration: 3000,
+      isClosable: true
+    })
+  }, [products, memberKey])
 
   return (
     <>
@@ -40,12 +101,12 @@ const DeleteInCart: FC = () => {
         <Flex minWidth="max-content" alignItems="center" gap="2">
           <BackButton />
           <Spacer />
-          <Heading>{`${title} >>${randomValueForScreen}<<`}</Heading>
+          <Heading p={8}>{`${title} >>${randomValueForScreen}<<`}</Heading>
         </Flex>
 
         <Card>
           <CardHeader>
-            <Heading size="md">Client Report</Heading>
+            <Heading size="md">제품외 정보</Heading>
           </CardHeader>
 
           <CardBody>
@@ -54,54 +115,58 @@ const DeleteInCart: FC = () => {
                 <Heading size="xs" textTransform="uppercase">
                   멤버 ID:
                 </Heading>
-                <Editable defaultValue="Take some chakra">
+                <Editable
+                  defaultValue={memberKey}
+                  onChange={(newValue) => setMemberKey(newValue)}
+                >
                   <EditablePreview />
                   <EditableInput />
                 </Editable>
               </Box>
-              <Box>
-                <Heading size="xs" textTransform="uppercase">
-                  Overview
-                </Heading>
-                <Text pt="2" fontSize="sm">
-                  Check out the overview of your clients.
-                </Text>
-              </Box>
-              <Box>
-                <Heading size="xs" textTransform="uppercase">
-                  Analysis
-                </Heading>
-                <Text pt="2" fontSize="sm">
-                  See a detailed analysis of all your business clients.
-                </Text>
-              </Box>
             </Stack>
           </CardBody>
-          <CardFooter>
+          <CardFooter justify="space-between">
+            <Center>
+              <Text fontWeight="bold">제품 수량: {products.length} 개</Text>
+            </Center>
             <ButtonGroup spacing="2">
               <Button
                 variant="outline"
                 colorScheme="blue"
                 leftIcon={<AddIcon />}
+                onClick={addProduct}
               >
                 Add
               </Button>
               <Button
-                variant="outline"
+                variant="ghost"
                 colorScheme="blue"
                 leftIcon={<DeleteIcon />}
+                onClick={allClearProducts}
               >
-                Remove
+                All clear
               </Button>
-              <Button variant="solid" colorScheme="blue">
-                Buy now
-              </Button>
-              <Button variant="ghost" colorScheme="blue">
+              <Button variant="solid" colorScheme="blue" onClick={onSend}>
                 Delete to cart
               </Button>
             </ButtonGroup>
           </CardFooter>
         </Card>
+        <Grid
+          gridTemplateColumns={['1fr', 'repeat(2, 1fr)', 'repeat(3, 1fr)']}
+          gridGap="10"
+          padding="5"
+        >
+          {products.map((product) => (
+            <GridItem key={product.id}>
+              <ProductCard
+                isDisableProductIdAndOptionCodeName={false}
+                onRemove={deleteProduct(product.id)}
+                {...product}
+              />
+            </GridItem>
+          ))}
+        </Grid>
       </Box>
     </>
   )
