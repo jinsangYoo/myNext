0761e43e0a4eb1a535acdadb5f27d43ee1f1dc84
jinsang <jinsang@nhndata.com>
2023-05-16 11:58:52 +0900

    Link 기능 화면 추가

---
+++ pages/menus/link.tsx
@@ -0,0 +1,135 @@
+import { Box, Flex, Spacer, Heading, useToast } from '@chakra-ui/react'
+import { Stack, StackDivider } from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+import { ButtonGroup, Button } from '@chakra-ui/react'
+import { Editable, EditablePreview, EditableInput } from '@chakra-ui/react'
+import Head from 'next/head'
+
+import BackButton from '@/components/BackButton'
+
+import React, { useEffect, useState, useCallback } from 'react'
+import { FC } from 'react'
+
+import {
+  AceConfiguration,
+  ACParams,
+  ACS,
+  ACEResponseToCaller,
+  ACProduct,
+  ACEGender,
+  ACEMaritalStatus
+} from '@jinsang/slimer-react'
+import { getRandomIntInclusive, sendCommonWithPromise } from '@/utils'
+
+const title = 'Link'
+const Link: FC = () => {
+  const [randomValueForScreen, setRandomValueForScreen] = useState(0)
+  const [pageHeading, setPageHeading] = useState<string>(`>>${title}<< >>-<<`)
+  useEffect(() => {
+    const ranValue = getRandomIntInclusive(0, 999)
+    setRandomValueForScreen(ranValue)
+    setPageHeading(`>>${title}<< >>${ranValue}<<`)
+    const params = ACParams.init(
+      ACParams.TYPE.EVENT,
+      `>>${title}<< >>${ranValue}<<`
+    )
+    sendCommonWithPromise(`>>${title}<< >>${ranValue}<<`, params)
+  }, [])
+
+  const [randomValue, setRandomValue] = useState(0)
+  useEffect(() => {
+    setRandomValue(getRandomIntInclusive(0, 999))
+  }, [])
+  const [url, setUrl] = useState<string>('URL_64')
+  const [memberKey, setMemberKey] = useState<string>(`멤버ID >>-<<`)
+  const [keyword, setKeyword] = useState<string>('Link >>-<<')
+  useEffect(() => {
+    setMemberKey(`멤버ID >>${randomValue}<<`)
+    setKeyword(`Link >>${randomValue}<<`)
+  }, [randomValue])
+
+  const toast = useToast()
+  const onSend = useCallback(() => {
+    const params = ACParams.init(ACParams.TYPE.LINK, url)
+    params.linkName = keyword
+    params.memberKey = memberKey
+    sendCommonWithPromise(url, params)
+
+    toast({
+      title: '알림',
+      description: params.name ?? params.type,
+      status: 'success',
+      duration: 3000,
+      isClosable: true
+    })
+  }, [url, memberKey, keyword])
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Box>
+        <Flex minWidth="max-content" alignItems="center" gap="2">
+          <BackButton />
+          <Spacer />
+          <Heading p={8}>{pageHeading}</Heading>
+        </Flex>
+
+        <Card>
+          <CardHeader>
+            <Heading size="md">Link 정보</Heading>
+          </CardHeader>
+
+          <CardBody>
+            <Stack divider={<StackDivider />} spacing="1">
+              <Box>
+                <Heading size="xs" textTransform="uppercase">
+                  이벤트명:
+                </Heading>
+                <Editable value={url} onChange={(newValue) => setUrl(newValue)}>
+                  <EditablePreview />
+                  <EditableInput />
+                </Editable>
+              </Box>
+              <Box>
+                <Heading size="xs" textTransform="uppercase">
+                  멤버 ID:
+                </Heading>
+                <Editable
+                  value={memberKey}
+                  onChange={(newValue) => setMemberKey(newValue)}
+                >
+                  <EditablePreview />
+                  <EditableInput />
+                </Editable>
+              </Box>
+              <Box>
+                <Heading size="xs" textTransform="uppercase">
+                  Link 명:
+                </Heading>
+                <Editable
+                  value={keyword}
+                  onChange={(newValue) => setKeyword(newValue)}
+                >
+                  <EditablePreview />
+                  <EditableInput />
+                </Editable>
+              </Box>
+            </Stack>
+          </CardBody>
+          <CardFooter justify="space-between">
+            <Spacer />
+            <ButtonGroup spacing="2">
+              <Button variant="solid" colorScheme="blue" onClick={onSend}>
+                Link
+              </Button>
+            </ButtonGroup>
+          </CardFooter>
+        </Card>
+      </Box>
+    </>
+  )
+}
+
+export default Link
